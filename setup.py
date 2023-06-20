6e073e1dd877cc7ec2f00e23f775592210b78fb8
jasonbrianhall@gmail.com <jbhall@fbi.gov>
2023-06-20 09:26:51 -0400

    Setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'willytheworm',
+    packages = ['willytheworm'],
+    version = '1.0',  # Ideally should be same as your github release tag varsion
+    description = 'Willy the Worm clone',
+    author = 'Jason Hall',
+    author_email = 'jasonbrianhall@gmail.com',
+    url = 'https://github.com/jasonbrianhall/willytheworm',
+    download_url = 'https://github.com/jasonbrianhall/willytheworm',
+    keywords = ['video game', 'pygame', 'arcade'],
+    classifiers = [],
+)
