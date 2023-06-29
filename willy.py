c1e8ebcc85387aaa2375fb613163d98d3bcb9694
jasonbrianhall@gmail.com <jbhall@fbi.gov>
2023-06-29 14:39:18 -0400

    Better jump score calculator

--- willy.py
+++ willy.py
@@ -1441,7 +1441,8 @@ def game(screen, currentlevel, level, SCALER, wasd=False, flash=True, numberofba
 				ballkilledwilly=True
 			y, x = willy_position
 			for i in range(1, 5):
-				if str(y + i) in level_data[currentlevel] and str(x) in level_data[currentlevel][str(y + i)] and (y+i)==row and x==col:
+				#if str(y + i) in level_data[currentlevel] and str(x) in level_data[currentlevel][str(y + i)] and (y+i)==row and x==col:
+				if (y+i)==row and x==col:
 					# Add 20 points to Willy's score here
 					score+=20
 					t = threading.Thread(target=play_audio, args=(mixerdict, "audio/boop.mp3",))
