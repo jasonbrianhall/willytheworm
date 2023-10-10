3c6e1174158ffd559084bc9d2957bcd39023a445
Jason Hall <jasonbrianhall@gmail.com>
2023-10-10 13:30:09 -0400

    comma

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
 
     author='Jason Brian Hall',
     author_email='jasonbrianhall@gmail.com',
-    description='Willy the Worm is a sprite based side-scrolling retro game that plays on a single screen.  The idea is to ring the bell on each level while avoiding balls, tacks, and falling into holes.  This game is a clone of a 1985 video game written by Alan Farme and plays extremely similar to the original game.'
+    description='Willy the Worm is a sprite based side-scrolling retro game that plays on a single screen.  The idea is to ring the bell on each level while avoiding balls, tacks, and falling into holes.  This game is a clone of a 1985 video game written by Alan Farme and plays extremely similar to the original game.',
     license='MIT',
     keywords='game worm fun',
     url='http://example.com/'
