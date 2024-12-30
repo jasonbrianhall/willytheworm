042ab68fcd9de2c8adcfca2bac5f5e9507eae0e1
Jason Hall <jasonbrianhall@gmail.com>
2024-12-30 09:44:19 -0500

    LATEST

--- buildandroid.sh
+++ buildandroid.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pip install -r requirements-android.txt 
-p4a create --requirements=kivy,sdl2 --bootstrap=sdl2 --arch=x86_64 --package=org.willytheworm --name="Willy The Worm" --version=1.0 --private=$(pwd)/willy.py --sdk-dir=$(pwd)/Android/Sdk
+
+p4a create --requirements=kivy,sdl2 --bootstrap=sdl2 --arch=x86_64 --package=org.test.willytheworm --name="WillyTheWorm" --version=1.0 --private=$(pwd)/willy.py --sdk-dir=$HOME/Android/Sdk
