7ba1fc7f47313a236dace96c9faaa80c18007134
Jason Hall <jasonbrianhall@gmail.com>
2024-12-30 10:27:36 -0500

    Latest

--- buildandroid.sh
+++ buildandroid.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "Very important, use NDK 25 or this will fail"
+
 pip install -r requirements-android.txt 
 
 #p4a clean_all
-p4a create --requirements=kivy,sdl2,pillow --bootstrap=sdl2 --arch=x86_64 \
+
+p4a create --requirements=kivy,sdl2 --bootstrap=sdl2 --arch=x86_64 \
     --package=org.test.willytheworm --name="WillyTheWorm" \
     --version=1.0 --private=$(pwd)/willy.py \
-    --sdk-dir=$HOME/Android/Sdk
+    --sdk-dir=$HOME/Android/Sdk \
+    --blacklist-requirements=android
