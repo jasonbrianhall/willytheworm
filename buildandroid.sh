53619c297b84788fa18f1bba1ab7ab386049bf24
Jason Hall <jasonbrianhall@gmail.com>
2024-12-30 10:29:11 -0500

    Latest

--- buildandroid.sh
+++ buildandroid.sh
@@ -10,4 +10,5 @@ p4a create --requirements=kivy,sdl2 --bootstrap=sdl2 --arch=x86_64 \
     --package=org.test.willytheworm --name="WillyTheWorm" \
     --version=1.0 --private=$(pwd)/willy.py \
     --sdk-dir=$HOME/Android/Sdk \
-    --blacklist-requirements=android
+    --blacklist-requirements=android \
+    --dist-name willytheworm
