7d9063002cefaa68687144709a409ca17612bcef
Jason Hall <jasonbrianhall@gmail.com>
2024-12-30 11:21:50 -0500

    Latest

--- buildandroid.sh
+++ buildandroid.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
 
-echo "Very important, use NDK 25 or this will fail"
+help() {
+   echo "Usage: $0 [OPTION]"
+   echo "Options:"
+   echo "  -h,  --help        Show help"
+   echo "  -ca, --cleanarm    Clean arm64 build"
+   echo "  -cx, --cleanx86    Clean x86_64 build" 
+   echo "  -a,  --arm         Build arm64 APK"
+   echo "  -x,  --x86         Build x86_64 APK"
+}
 
-pip install -r requirements-android.txt 
-#export ANDROID_NDK_HOME=~/Android/Sdk/ndk/25.1.8937393
+check_ndk() {
+   if [ -z "$ANDROID_NDK_HOME" ]; then
+       echo "ANDROID_NDK_HOME not set"
+       exit 1
+   fi
+   
+   NDK_VERSION=$(basename "$ANDROID_NDK_HOME" | cut -d'.' -f1)
+   if [ "$NDK_VERSION" != "25" ]; then
+       echo "NDK version 25 required, found version $NDK_VERSION"
+       exit 1
+   fi
+}
 
-#p4a clean_all
+clean() {
+   local arch=$1
+   p4a clean_all
+   rm -rf build-$arch
+}
 
+build() {
+   local arch=$1
+   local build_dir="build-$arch"
+   local apk_name="WillyTheWorm-$arch.apk"
 
-cp willy.py main.py -f
+   check_ndk
+   cp willy.py main.py -f
+   p4a apk --requirements=kivy,sdl2,pillow \
+       --bootstrap=sdl2 \
+       --arch=$arch \
+       --package=org.test.willytheworm \
+       --name="Willy the Worm" \
+       --version=1.0 \
+       --private=$(pwd) \
+       --sdk-dir=$HOME/Android/Sdk \
+       --dist-name willytheworm-$arch \
+       --storage-dir=$(pwd)/$build_dir \
+       --ignore-setup-py \
+       --copy-to ./$apk_name
+}
 
-p4a apk --requirements=kivy,sdl2,pillow \
-    --bootstrap=sdl2 \
-    --arch=x86_64 \
-    --package=org.willytheworm \
-    --name="WillyTheWorm" \
-    --version=1.0 \
-    --private=$(pwd) \
-    --sdk-dir=$HOME/Android/Sdk \
-    --dist-name willytheworm \
-    --ignore-setup-py
-
+case "$1" in
+   "-h"|"--help") help;;
+   "-ca"|"--cleanarm") clean "arm64";;
+   "-cx"|"--cleanx86") clean "x86_64";;
+   "-a"|"--arm") build "arm64-v8a";;
+   "-x"|"--x86") build "x86_64";;
+   *) help;;
+esac
