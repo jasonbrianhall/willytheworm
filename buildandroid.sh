d6d2393e59e6163e13ab08783fe1e1ac3c221e0b
Jason Hall <jasonbrianhall@gmail.com>
2024-12-30 11:09:00 -0500

    Latest

--- buildandroid.sh
+++ buildandroid.sh
@@ -3,8 +3,10 @@
 echo "Very important, use NDK 25 or this will fail"
 
 pip install -r requirements-android.txt 
+#export ANDROID_NDK_HOME=~/Android/Sdk/ndk/25.1.8937393
 
 #p4a clean_all
+
 
 cp willy.py main.py -f
 
