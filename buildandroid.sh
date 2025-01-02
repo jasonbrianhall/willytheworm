934350dfcd2d2b2ed8c7ab179e352be28b2bc825
Jason Hall <jasonbrianhall@gmail.com>
2025-01-02 13:11:22 -0500

    Latest

--- buildandroid.sh
+++ buildandroid.sh
@@ -12,10 +12,10 @@ cp willy.py main.py -f
 
 pip install -r requirements-android.txt
 
-ARCH="arm64-v8a"  # x86_64
-#ARCH="x86_64"
+#ARCH="arm64-v8a"  # x86_64
+ARCH="x86_64"
 
-p4a apk --requirements=kivy,sdl2,pygame \
+p4a apk --requirements=kivy,sdl2 \
     --bootstrap=sdl2 \
     --arch=$ARCH \
     --package=org.willytheworm \
